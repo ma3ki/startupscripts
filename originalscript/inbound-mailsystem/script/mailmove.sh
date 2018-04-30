76739242b886aebea79b005aec0eff174cfb3e27
root <root@vps.ma3ki.net>
2018-04-30 11:21:52 +0900

    update

--- originalscript/inbound-mailsystem/script/mailmove.sh
+++ originalscript/inbound-mailsystem/script/mailmove.sh
@@ -10,13 +10,13 @@ MBOX="$4"
 TAG=mailmove
 PRI=mail.info
 
-doveadm move -u ${FROM} "${MBOX}" user ${TO} mailbox INBOX header message-id "${MID}" since 1mins >/dev/null 2>&1
+RESULT=$(doveadm move -u ${FROM} "${MBOX}" user ${TO} mailbox INBOX header message-id "${MID}" since 1mins 2>&1)
 
 if [ $? -eq 0 ]
 then
-	logger -t ${TAG} -p ${PRI} "from=<${FROM}>, mbox=${MBOX}, to=<${TO}>, mid=<${MID}>, state=success"
+	logger -t ${TAG} -p ${PRI} "from=<${FROM}>, mbox=${MBOX}, to=<${TO}>, mid=${MID}, state=success"
 else
-	logger -t ${TAG} -p ${PRI} "from=<${FROM}>, mbox=${MBOX}, to=<${TO}>, mid=<${MID}>, state=failure"
+	logger -t ${TAG} -p ${PRI} "from=<${FROM}>, mbox=${MBOX}, to=<${TO}>, mid=${MID}, state=failure, errmsg=${RESULT}"
 fi
 
 exit 0
