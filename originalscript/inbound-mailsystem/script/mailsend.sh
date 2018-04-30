d1ad1d035c3c07fe09bef632c284f69ad0a56b06
root <root@vps.ma3ki.net>
2018-04-30 11:30:52 +0900

    update

--- originalscript/inbound-mailsystem/script/mailsend.sh
+++ originalscript/inbound-mailsystem/script/mailsend.sh
@@ -6,13 +6,13 @@ TO="$2"
 TAG=mailsend
 PRI=mail.info
 
-cat - | /usr/sbin/sendmail -i -f ${FROM} ${TO} >/dev/null 2>&1
+RESULT=$(cat - | sudo /usr/sbin/sendmail -i -f ${FROM} ${TO} 2>&1)
 
 if [ $? -eq 0 ]
 then
 	logger -t ${TAG} -p ${PRI} "from=<${FROM}>, to=<${TO}>, state=success"
 else
-	logger -t ${TAG} -p ${PRI} "from=<${FROM}>, to=<${TO}>, state=failure"
+	logger -t ${TAG} -p ${PRI} "from=<${FROM}>, to=<${TO}>, state=failure, errmsg=${RESULT}"
 fi
 
 exit 0
