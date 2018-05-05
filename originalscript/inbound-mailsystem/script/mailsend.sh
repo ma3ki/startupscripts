c17c362aa2b085f025b6ddd08c7e3a17b7812005
root <root@vps.ma3ki.net>
2018-05-05 12:55:17 +0900

    updat

--- originalscript/inbound-mailsystem/script/mailsend.sh
+++ originalscript/inbound-mailsystem/script/mailsend.sh
@@ -5,24 +5,16 @@ TO="$2"
 
 TAG=mailsend
 PRI=mail.info
-PID=$$
-QD=/var/dovecot/queue
-
-mkdir -p ${QD}
-cat - > ${QD}/senddata.${PID}
 
 # create queue
-cat <<_EOL_> ${QD}/sendq.${PID}
-cat ${QD}/senddata.${PID} | /usr/sbin/sendmail -i -f ${FROM} ${TO} >/dev/null 2>&1
+cat - | /usr/sbin/sendmail -i -f ${FROM} ${TO} >/dev/null 2>&1
 
 if [ $? -eq 0 ]
 then
 	logger -t ${TAG} -p ${PRI} "from=<${FROM}>, to=<${TO}>, state=success"
-	rm -f ${QD}/sendq.${PID} ${QD}/senddata.${PID}
 else
 	logger -t ${TAG} -p ${PRI} "from=<${FROM}>, to=<${TO}>, state=failure"
 fi
-_EOL_
 
 exit 0
 
