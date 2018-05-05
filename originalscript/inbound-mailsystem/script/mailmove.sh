7ff6bad38f33c22dce13d6d29a2d692febc9b548
root <root@vps.ma3ki.net>
2018-05-05 13:15:40 +0900

    update

--- originalscript/inbound-mailsystem/script/mailmove.sh
+++ originalscript/inbound-mailsystem/script/mailmove.sh
@@ -14,17 +14,34 @@ QD=/var/dovecot/queue
 
 mkdir -p ${QD}
 
+# create mailbox
+doveadm mailbox status -u ${FROM} all "${MBOX}"
+
+if [ $? -ne 0 ]
+then
+	doveadm mailbox create -u ${FROM} "${MBOX}"
+	doveadm mailbox subscribe -u ${FROM} "${MBOX}"
+fi
+
 # create queue
 cat <<_EOL_> ${QD}/moveq.${PID}
-doveadm move -u ${FROM} "${MBOX}" user ${TO} mailbox INBOX header message-id "${MID}" since 2mins >/dev/null 2>&1
-
-if [ $? -eq 0 ]
+# check duplicate
+COUNT=$(doveadm search -u ${FROM} mailbox "${MBOX}" header message-id "${MID}" since 2mins | wc -l)
+if [ ${COUNT} -eq 0 ]
 then
-	logger -t ${TAG} -p ${PRI} "from=<${FROM}>, mbox=${MBOX}, to=<${TO}>, mid=${MID}, state=success"
-	rm -f ${QD}/moveq.${PID}
+	doveadm move -u ${FROM} "${MBOX}" user ${TO} mailbox INBOX header message-id "${MID}" since 2mins >/dev/null 2>&1
+
+	if [ $? -eq 0 ]
+	then
+		logger -t ${TAG} -p ${PRI} "from=<${FROM}>, mbox=${MBOX}, to=<${TO}>, mid=${MID}, state=success"
+		rm -f ${QD}/moveq.${PID}
+	else
+		logger -t ${TAG} -p ${PRI} "from=<${FROM}>, mbox=${MBOX}, to=<${TO}>, mid=${MID}, state=failure"
+	fi
 else
-	logger -t ${TAG} -p ${PRI} "from=<${FROM}>, mbox=${MBOX}, to=<${TO}>, mid=${MID}, state=failure"
+	logger -t ${TAG} -p ${PRI} "from=<${FROM}>, mbox=${MBOX}, to=<${TO}>, mid=${MID}, state=skip"
 fi
+
 _EOL_
 
 exit 0
