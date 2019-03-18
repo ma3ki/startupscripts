8b3d0c29d7db19d82fc958f3a3db451e5b527a37
root <root@vps.ma3ki.net>
2019-03-18 14:25:24 +0900

    update

--- publicscript/mailsystem2/setup_scripts/_02_certbot.sh
+++ publicscript/mailsystem2/setup_scripts/_02_certbot.sh
@@ -3,29 +3,18 @@
 source $(dirname $0)/../config.source
 echo "---- $0 ----"
 
-domain=${FIRST_DOMAIN}
-
 #-- certbot のインストール
 yum install -y certbot certbot-dns-sakuracloud
 
 #-- wildcard 証明書のインストール
-#-- (Y)es/(N)o:
-#certbot certonly --dns-sakuracloud \
-#  --dns-sakuracloud-credentials ~/.sakura \
-#  --dns-sakuracloud-propagation-seconds 60 \
-#  -d *.${domain} -d ${domain} \
-#  -m admin@${domain} \
-#  --manual-public-ip-logging-ok \
-#  --agree-tos
-
 expect -c "
 set timeout 180
-spawn certbot certonly --dns-sakuracloud --dns-sakuracloud-credentials ~/.sakura --dns-sakuracloud-propagation-seconds 60 -d *.${domain} -d ${domain} -m admin@${domain} --manual-public-ip-logging-ok --agree-tos
+spawn certbot certonly --dns-sakuracloud --dns-sakuracloud-credentials /root/.sakura --dns-sakuracloud-propagation-seconds 60 -d *.${FIRST_DOMAIN} -d ${FIRST_DOMAIN} -m admin@${FIRST_DOMAIN} --manual-public-ip-logging-ok --agree-tos
 expect \"(Y)es/(N)o:\"
 send \"Y\n\"
-expect \"Congratulations\"
-exit 0
 "
+
+ls -l /etc/letsencrypt/live/${FIRST_DOMAIN}/fullchain.pem
 
 #-- cron に証明書の更新処理を設定
 echo "$((${RANDOM}%60)) $((${RANDOM}%24)) * * $((${RANDOM}%7)) root certbot renew --post-hook 'systemctl reload nginx postfix'" > /etc/cron.d/certbot-auto
