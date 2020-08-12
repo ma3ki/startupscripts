cedfb3b695f60e9291a0c436c179c6a698804307
ma3ki <nemomasa@gmail.com>
2020-08-13 00:45:57 +0900

    update

--- publicscript/mailsystem_centos8/setup_scripts/_13_roundcube.sh
+++ publicscript/mailsystem_centos8/setup_scripts/_13_roundcube.sh
@@ -76,7 +76,7 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-sed -e 's/suggest/require/' -e 's/ required .*/"/'  composer.json-dist > composer.json
+sed -e 's/suggest/require/' -e 's/ required .*/"/' composer.json-dist | perl -ne '{if(/net_ldap3/){chomp; print "$_,\n";}else{print;}}' > composer.json
 php composer.phar install --no-dev
 bin/install-jsdeps.sh
 
