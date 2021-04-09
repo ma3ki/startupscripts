37469fe3d6dec6577f7cc04d8433d9612ef1b836
ma3ki <nemomasa@gmail.com>
2021-04-09 10:16:55 +0900

    update

--- publicscript/mailsystem_centos8/setup_scripts/_01_dns.sh
+++ publicscript/mailsystem_centos8/setup_scripts/_01_dns.sh
@@ -22,6 +22,7 @@ do
   usacloud dns record-add -y --name _adsp._domainkey --type TXT --value "dkim=discardable" ${domain}
   usacloud dns record-add -y --name _mta-sts --type TXT --value "v=STSv1; id=$(date +%Y%m%d%H%M%S);" ${domain}
   usacloud dns record-add -y --name _smtp._tls --type TXT --value "v=TLSRPTv1; rua=mailto:sts-report@${domain}" ${domain}
+  usacloud dns record-add -y --name _autodiscover._tcp --type SRV --srv-priority 5 --srv-port 443 --srv-target "${FIRST_DOMAIN}." ${domain}
   if [ "${FIRST_DOMAIN}" = "${domain}" ]
   then
     usacloud dns record-add -y --name autoconfig --type A --value ${IPADDR} ${domain}
